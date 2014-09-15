9002411772b1cfd83a7b2c2733770e36ac999539
Derek Barncard <derek@barncard.com>
2014-09-14 21:25:40 -0700

    Added new function getNewTweets, then a timer utility to count the current new Tweets and display in title and alert

--- data_generator.js
+++ data_generator.js
@@ -44,6 +44,7 @@ var generateRandomTweet = function(){
   tweet.message = randomMessage();
   tweet.created_at = new Date();
   addTweet(tweet);
+  //console.log("New tweet generated");
 };
 
 for(var i = 0; i < 10; i++){
